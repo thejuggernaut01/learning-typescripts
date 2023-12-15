74a59e3dd67c0cade91ba8ab6022b000726394a8
thejuggernaut01 <adewaleolanrewaju2002@gmail.com>
2023-12-15 18:00:32 +0100

    learning #18: Conditional Types in Typescripts

---
+++ TS-BASICS/conditionals.ts
@@ -0,0 +1,41 @@
+import fetch from "node-fetch";
+
+interface PokemonResults {
+  count: number;
+  next?: string;
+  previous?: string;
+  results: {
+    name: string;
+    url: string;
+  }[];
+}
+
+type FetchPokemonResult<T> = T extends undefined
+  ? Promise<PokemonResults>
+  : void;
+
+function fetchPokemon<T extends undefined | ((data: PokemonResults) => void)>(
+  url: string,
+  cb?: T
+): FetchPokemonResult<T> {
+  if (cb) {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then(cb);
+    return undefined as FetchPokemonResult<T>;
+  } else {
+    return fetch(url).then((resp) => resp.json()) as FetchPokemonResult<T>;
+  }
+}
+
+// fetchPokemon("https://pokeapi.co/api/v2/pokemon?limit=10", (data) => {
+//   data.results.forEach((pokemon) => console.log(pokemon.name));
+// });
+
+(async function () {
+  const data = <PokemonResults>(
+    await fetchPokemon("https://pokeapi.co/api/v2/pokemon?limit=10")
+  );
+
+  data.results.forEach((pokemon) => console.log(pokemon.name));
+});
